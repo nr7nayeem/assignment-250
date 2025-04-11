ce478a10db9be861b412d6a7b1d79eb3acba18f2
Naymur Rahman <naymur18rahman@gmail.com>
2025-04-11 17:08:17 +0600

    Update Assignment 6 of 5.f90

--- Assignment 6 of 5.f90
+++ Assignment 6 of 5.f90
@@ -3,23 +3,14 @@ program combination_permutation
     integer :: n, r
     integer :: resultP, resultC
 
-    ! Calculate 10P6
-    n = 10
-    r = 6
-    resultP = nPr(n, r)
-    print *, '10P6 = ', resultP
+    ! Calculate and print permutations (10P6)
+    print *, '10P6 = ', factorial(10) / factorial(10 - 6)
 
-    ! Calculate 10C4
-    n = 10
-    r = 4
-    resultC = nCr(n, r)
-    print *, '10C4 = ', resultC
+    ! Calculate and print combination (10C4)
+    print *, '10C4 = ', factorial(10) / (factorial(4) * factorial(10 - 4))
 
-    ! Calculate 10C6
-    n = 10
-    r = 6
-    resultC = nCr(n, r)
-    print *, '10C6 = ', resultC
+    ! Calculate and print combination (10C6)
+    print *, '10C6 = ', factorial(10) / (factorial(6) * factorial(10 - 6))
 
 contains
 
@@ -31,21 +22,7 @@ contains
         if (x == 0 .or. x == 1) then
             fact = 1
         else
-            fact = x * factorial(x-1)
+            fact = x * factorial(x - 1)
         end if
     end function factorial
-
-    ! Function to calculate nCr
-    integer function nCr(n, r)
-        integer, intent(in) :: n, r
-        nCr = factorial(n) / (factorial(r) * factorial(n - r))
-    end function nCr
-
-    ! Function to calculate nPr
-    integer function nPr(n, r)
-        integer, intent(in) :: n, r
-        nPr = factorial(n) / factorial(n - r)
-    end function nPr
-
 end program combination_permutation
-
