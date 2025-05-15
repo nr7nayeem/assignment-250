2cd17009485cc6b47cc3772db9f6fddcdd6a59f7
Naymur Rahman <naymur18rahman@gmail.com>
2025-05-15 22:43:52 +0600

    Update assignment 2 of 5.f90

--- assignment 2 of 5.f90
+++ assignment 2 of 5.f90
@@ -1,29 +1,29 @@
-program Assignment_2
+program pp
     implicit none
-    real::n,H,day ,M
-    write(*,"(A,10X,A,10X,A)")"number","Hn" , "day"
-    do n=1,8
+    integer, parameter :: k = selected_int_kind(21)
+    real :: l
+    integer(k) :: i, days
+    do i = 1, 64
+        l = 5.0 *H(i)/ 86400.0
+        days = int(l + 0.9999999, kind=k)
+        write(*,"(I4,3x,I20,3x,I20)") i, H(i), days
 
-    H= hanoi(n**2.)
-    day=H*(5./86400.)
-    write(*,"(F4.1,6x ,Es10.2,6X, E10.3)")n**2, H,day
     end do
-    write(*,*)"the relation between the Hn and Mn "
-    do n=1,8
-        H= hanoi(n)
-        M=2**n-1
-        write(*,"(I5,5X , I5)")int(H),int(M)
+    print*," connection relation and solution "
+    do i=1, 64
+        write(*,"(I4,3x,I20,3x,I20)") i, H(i), 2**i-1
     end do
+
 contains
-    recursive function hanoi(n) result(f)
-    real,intent(in) :: n
-    real:: f
-    if(n==1)then
-        f=1.
-    else
-        f=2.*hanoi(n-1.)+1.
-    end if
-    end function
-end program
+    recursive function H(x) result(t)
+        integer(k) :: t
+        integer(k), intent(in) :: x
+        if (x == 1) then
+            t = 1
+        else
+            t = 2 * H(x - 1) + 1
+        end if
+    end function H
+end program pp
 
 
