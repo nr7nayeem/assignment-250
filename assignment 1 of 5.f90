42d56de93254d726dd30e2c30ab45554dd3a627e
Naymur Rahman <naymur18rahman@gmail.com>
2025-05-16 22:33:05 +0600

    Assignment Solution 5

---
+++ assignment 1 of 5.f90
@@ -0,0 +1,48 @@
+program mersenne_primes
+    implicit none
+    integer, parameter :: ik = selected_int_kind(18)
+    integer(ik) :: n, m
+    logical :: is_prime
+
+    print *, "Mersenne Number Classification (M_n = 2^n - 1):"
+    print *, "----------------------------------------------"
+
+    do n = 0, 69
+        m = 2_ik**n - 1_ik
+        if (isMersennePrime(m)) then
+            print *, "M(", n, ") =", m, " --> Prime"
+        else
+            print *, "M(", n, ") =", m, " --> Not Prime"
+        end if
+    end do
+
+contains
+
+    logical function isMersennePrime(x)
+        integer(ik), intent(in) :: x
+        integer(ik) :: i
+        real(8) :: root
+
+        if (x <= 1) then
+            isMersennePrime = .false.
+            return
+        end if
+
+        if (mod(x, 2) == 0) then
+            isMersennePrime = .false.
+            return
+        end if
+
+        root = sqrt(real(x, kind=8))
+        do i = 3, int(root), 2
+            if (mod(x, i) == 0) then
+                isMersennePrime = .false.
+                return
+            end if
+        end do
+
+        isMersennePrime = .true.
+    end function isMersennePrime
+
+end program mersenne_primes
+
---
+++ assignment 2 of 5.f90
@@ -0,0 +1,29 @@
+program Assignment_2
+    implicit none
+    real::n,H,day ,M
+    write(*,"(A,10X,A,10X,A)")"number","Hn" , "day"
+    do n=1,8
+
+    H= hanoi(n**2.)
+    day=H*(5./86400.)
+    write(*,"(F4.1,6x ,Es10.2,6X, E10.3)")n**2, H,day
+    end do
+    write(*,*)"the relation between the Hn and Mn "
+    do n=1,8
+        H= hanoi(n)
+        M=2**n-1
+        write(*,"(I5,5X , I5)")int(H),int(M)
+    end do
+contains
+    recursive function hanoi(n) result(f)
+    real,intent(in) :: n
+    real:: f
+    if(n==1)then
+        f=1.
+    else
+        f=2.*hanoi(n-1.)+1.
+    end if
+    end function
+end program
+
+
