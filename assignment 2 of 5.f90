6f35f53acf913a4c5e09b9cea026a4b269e47d8e
Naymur Rahman <naymur18rahman@gmail.com>
2025-05-19 20:28:39 +0600

    Update assignment 2 of 5.f90

--- assignment 2 of 5.f90
+++ assignment 2 of 5.f90
@@ -1,27 +1,22 @@
-program Assignment_2
+program p
     implicit none
-    real::n,H,day ,M
-    write(*,"(A,10X,A,10X,A)")"number","Hn" , "day"
-    do n=1,8
-
-    H= hanoi(n**2.)
-    day=H*(5./86400.)
-    write(*,"(F4.1,6x ,Es10.2,6X, E10.3)")n**2, H,day
+    integer,parameter::ik=selected_int_kind(21)
+    integer(ik)::n,H,D
+    real::l
+    do n=1,64
+        H=Hanoi(n)
+        l=(5.*H)/86400
+        D=int(l+0.99999999999,kind=ik)
+        write(*,*) n, H,D
     end do
-    write(*,*)"the relation between the Hn and Mn "
-    do n=1,8
-        H= hanoi(n)
-        M=2**n-1
-        write(*,"(I5,5X , I5)")int(H),int(M)
-    end do
-contains
-    recursive function hanoi(n) result(f)
-    real,intent(in) :: n
-    real:: f
-    if(n==1)then
-        f=1.
-    else
-        f=2.*hanoi(n-1.)+1.
+    contains
+    recursive function hanoi(x) result(t)
+    integer(ik),intent(in) :: x
+    integer (ik)::t
+    if(x==1)then
+        t=1
+        else
+        t=2*hanoi(x-1)+1
     end if
     end function
 end program
